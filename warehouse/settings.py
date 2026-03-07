a4de317e853162e9f1089e2a56de073482a5e33a
Tina Nosrati <65159726+tinrt@users.noreply.github.com>
2026-03-07 16:50:14 +0000

    logo fixed

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_REDIRECT_URL = "donation-create"
 LOGOUT_REDIRECT_URL = "login"
